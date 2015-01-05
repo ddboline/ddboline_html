a1bd84aeb52192ae6a96e071bbdf08975f65aa2c
Daniel Boline <daniel.boline@stonybrook.edu>
2015-01-05 15:15:30 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -4,6 +4,6 @@ sudo apt-get install -y apache2
 
 sudo a2enmod userdir
 
-sudo a2enmod ssl
-sudo mkdir /etc/apache2/ssl
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
+#sudo a2enmod ssl
+#sudo mkdir /etc/apache2/ssl
+#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt
