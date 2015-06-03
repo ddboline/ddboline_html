02c15aa479ec798ad2f033c38e26677c3931bccc
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-03 09:41:49 -0400

    ...

--- cgi-bin/control_remcom.py
+++ cgi-bin/control_remcom.py
@@ -3,12 +3,8 @@
 # enable debugging
 import cgi
 import cgitb
-from urlparse import urlparse
-import os
 cgitb.enable()
 cgitb.enable(display=0, logdir='/tmp')
-
-import socket
 
 from control_roku import get_output
 
@@ -19,4 +15,4 @@ if __name__ == '__main__':
         print "<H1>Error</H1>"
         print "Please fill in the cmd field."
 
-    get_output(form['cmd'].value, socketfile = '/tmp/.remcom_test_socket')
+    get_output(form['cmd'].value, socketfile='/tmp/.remcom_test_socket')
